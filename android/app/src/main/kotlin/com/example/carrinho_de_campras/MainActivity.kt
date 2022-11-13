6b42d00f0fa092f01576f7bb1169313a8e61836e
Wellington Brito <wellington.brito@rocketmail.com>
2022-11-13 00:27:09 -0300

    "Iniciando projeto carrinho de compras"

---
+++ android/app/src/main/kotlin/com/example/carrinho_de_campras/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.carrinho_de_campras
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/projeto/index.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CarrinhoDeCompras extends StatelessWidget {
+  const CarrinhoDeCompras({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Carrinho de compras",
+        ),
+      ),
+    );
+  }
+}
